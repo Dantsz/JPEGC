722d3744ac6b77dcdb68628da372431e8e042861
Dantsz <danscostinas@gmail.com>
2023-03-19 08:59:42 +0200

    Add project files.

---
+++ JPEGC/JPEGC.cpp
@@ -0,0 +1,18 @@
+﻿// JPEGC.cpp : Defines the entry point for the application.
+//
+
+#include "JPEGC.h"
+#include "opencv2/opencv.hpp"
+#include "opencv2/imgproc.hpp"
+#include "opencv2/imgcodecs.hpp"
+#include "opencv2/highgui.hpp"
+using namespace std;
+
+int main()
+{
+	cout << "'Hello CMake." << endl;
+	cv::Mat_<uchar> img(256, 256, static_cast<uchar>(0));
+	cv::imshow("a", img);
+	
+	return 0;
+}
