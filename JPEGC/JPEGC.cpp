4853c05c2d84732162f3ee8f6ee5f5232c96c17b
Dantsz <danscostinas@gmail.com>
2023-03-25 13:01:27 +0200

    Add argument parser

--- JPEGC/JPEGC.cpp
+++ JPEGC/JPEGC.cpp
@@ -1,15 +1,33 @@
 ﻿// JPEGC.cpp : Defines the entry point for the application.
 //
 
-#include "JPEGC.h"
 #include <opencv2/opencv.hpp>
+#include <argumentum/argparse.h>
+#include <string>
+#include <format>
+using namespace argumentum;
 
-
-int main()
+struct defaults
 {
-	std::cout << "'Hello CMake." << std::endl;
-	cv::Mat_<uchar> img(256, 256, static_cast<uchar>(0));
-	cv::imshow("a", img);
-	cv::waitKey();
+	static constexpr std::string_view default_destination_path = "./res.bjpeg";
+};
+
+
+
+
+int main(int argc, char** argv)
+{
+	std::string src_path{};
+	std::string dst_path{};
+
+	auto parser = argument_parser{};
+	auto params = parser.params();
+
+	params.add_parameter(src_path, "src").nargs(1).metavar("SrcPath").help("Path to source image");
+	params.add_parameter(dst_path, "dst").minargs(0).metavar("DstPath").absent(defaults::default_destination_path.data()).help("Path to destination image");
+
+	if (!parser.parse_args(argc, argv, 1))
+		return 1;
+	std::cout << std::format("Compressing {} -> {}",src_path,dst_path);
 	return 0;
 }
