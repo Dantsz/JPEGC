61826aa36442113d36be2253d157f9bdbf8f34d7
Dantsz <danscostinas@gmail.com>
2023-04-10 23:56:30 +0300

    Add chunk_image

--- JPEGC/JPEGC.cpp
+++ JPEGC/JPEGC.cpp
@@ -20,16 +20,16 @@ int main(int argc, char** argv)
 {
 	std::string src_path{};
 	std::string dst_path{};
-
+	bool decompress = false;
 	auto parser = argument_parser{};
 	auto params = parser.params();
-
+	params.add_parameter(decompress, "--decompress", "-d").nargs(0).help("Compress or decompress (Default: compress)");
 	params.add_parameter(src_path, "src").nargs(1).metavar("SrcPath").help("Path to source image");
 	params.add_parameter(dst_path, "dst").minargs(0).metavar("DstPath").absent(defaults::default_destination_path.data()).help("Path to destination image");
 
 	if (!parser.parse_args(argc, argv, 1))
 		return 1;
-	std::cout << std::format("Compressing {} -> {}",src_path,dst_path);
+	std::cout << std::format("Compressing/Decompressing({}) {} -> {}",decompress,src_path,dst_path) << std::endl;
 
 	cv::Mat_<cv::Vec3b> img = cv::imread(src_path);
 	const auto [y, u, v] = transform_bgr_to_yuv_split(img);
@@ -39,6 +39,9 @@ int main(int argc, char** argv)
 	cv::imshow("Y", y);
 	cv::imshow("U", u);
 	cv::imshow("V", v);
+
+	const auto chunks_of_Y = chunk_image(y);
+	std::cout << std::format("Image contains {} chunks ({} rows, {} cols)", chunks_of_Y.size() * chunks_of_Y[0].size(), chunks_of_Y.size(), chunks_of_Y[0].size()) << std::endl;
 	cv::waitKey();
 	return 0;
 }
--- JPEGC/imageproc.ixx
+++ JPEGC/imageproc.ixx
@@ -1,6 +1,7 @@
 #include <tuple>
 #include <opencv2/opencv.hpp>
 #include <array>
+#include <vector>
 export module imageproc;
 using SIMG = cv::Mat_<unsigned char>;
 using TIMG = cv::Mat_<cv::Vec3b>;
@@ -12,4 +13,31 @@ export std::tuple<SIMG, SIMG, SIMG> transform_bgr_to_yuv_split(const TIMG img)
 	std::array<SIMG, 3> splits;
 	cv::split(yuv, splits.data());
 	return std::make_tuple(splits[0], splits[1], splits[2]);
+}
+
+export template<typename T>
+bool isInside(const cv::Mat_ <T>& mat, int i, int j)
+{
+	return i >= 0 && j >= 0 && i < mat.rows && j < mat.cols;
+}
+
+export template<int ROWS = 8, int COLS = 8>
+std::vector<std::vector<SIMG>> chunk_image(const SIMG& src_image)
+{
+	const int chunk_mat_rows = src_image.rows / ROWS + (src_image.rows % ROWS == 0 ? 0 : 1);
+	const int chunk_mat_cols = src_image.cols / COLS + (src_image.cols % COLS == 0 ? 0 : 1);
+	std::vector<std::vector<SIMG>> chunk_mat{};
+	chunk_mat.resize(chunk_mat_rows);
+	for (auto i = 0; i < chunk_mat_rows; i++)
+	{
+		chunk_mat[i].resize(chunk_mat_cols, SIMG(ROWS, COLS));
+	}
+	for (int i = 0; i < src_image.rows; i++)
+	{
+		for (int j = 0; j < src_image.cols; j++)
+		{
+			chunk_mat[i / ROWS][j / COLS]((i % ROWS), (j % COLS)) = src_image(i, j);
+		}
+	}
+	return chunk_mat;
 }
